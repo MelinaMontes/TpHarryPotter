e12094cdd111fe5ad84bee99b3349c80c33cda82
lumaldonado <lulimaldonado98@hotmail.com>
2021-04-12 18:45:04 -0300

    Commit de prueba

--- src/app/personajes/Elfo.java
+++ src/app/personajes/Elfo.java
@@ -1,9 +1,45 @@
 package app.personajes;
 import app.IHaceMagia;
+import app.Poder;
 import app.artefactos.Artefacto;
 
 public class Elfo extends Criatura  implements IHaceMagia{
     public int energiaMagica;
     public Artefacto artefacto;
-    public List<Hechizo> hechizos = new ArrayList<>(); 
+    public List<Hechizo> hechizos = new ArrayList<>();
+    @Override
+    public int getEnergiaMagica() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    @Override
+    public void setEnergiaMagica() {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public Poder getPoderInicial() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    @Override
+    public void setPoder() {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public Artefacto getArtefacto() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    @Override
+    public void aprender() {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void atacar() {
+        // TODO Auto-generated method stub
+        
+    } 
 }
--- src/app/personajes/Wizard.java
+++ src/app/personajes/Wizard.java
@@ -1,5 +1,6 @@
 package app.personajes;
 import app.IHaceMagia;
+import app.Poder;
 import app.artefactos.Artefacto;
 
 public class Wizard extends Personaje implements IHaceMagia {
@@ -13,5 +14,54 @@ public class Wizard extends Personaje implements IHaceMagia {
      public boolean magoOscuro(){
          
      
+     }
+
+
+     @Override
+     public int getEnergiaMagica() {
+          // TODO Auto-generated method stub
+          return 0;
+     }
+
+
+     @Override
+     public void setEnergiaMagica() {
+          // TODO Auto-generated method stub
+          
+     }
+
+
+     @Override
+     public Poder getPoderInicial() {
+          // TODO Auto-generated method stub
+          return null;
+     }
+
+
+     @Override
+     public void setPoder() {
+          // TODO Auto-generated method stub
+          
+     }
+
+
+     @Override
+     public Artefacto getArtefacto() {
+          // TODO Auto-generated method stub
+          return null;
+     }
+
+
+     @Override
+     public void aprender() {
+          // TODO Auto-generated method stub
+          
+     }
+
+
+     @Override
+     public void atacar() {
+          // TODO Auto-generated method stub
+          
      }
 }
