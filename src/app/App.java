133b8d491eb3acae7f3d63749e1fc193f153d306
lumaldonado <lulimaldonado98@hotmail.com>
2021-04-19 22:15:02 -0300

    Sigue haciendo el error pero lo volvi a como estaba antes de que lo toquetearamos por las dudas

--- src/app/App.java
+++ src/app/App.java
@@ -43,16 +43,11 @@ public class App {
         System.out.println();
         System.out.println();
         
-        JuegoHP juego = new juegoHp();
 
         System.out.println("Jugador 1, seleccione avatar: '1' Harry, '2' Voldemort, '3' Hermione, '4' Bellatrix, '5' Dobby, '6' Kreacher"); //aca irian las opciones a elegir, dependiendo de los personajes instanciados
-
         int avatar1 = Teclado.nextInt();
-
-
         final String avatarElegido = "El avatar elegido es ";
 
-        int avatar1 = Teclado.nextInt();
         switch(avatar1){
             case 1: System.out.println(avatarElegido + "Harry");
             break;
@@ -66,7 +61,7 @@ public class App {
             break;
             case 6: System.out.println(avatarElegido + "Kreacher");
             break;
-            
+        }
 
         System.out.println("Jugador 2, seleccione avatar: '1' Harry, '2' Voldemort, '3' Hermione, '4' Bellatrix, '5' Dobby, '6' Kreacher"); //aca irian las opciones a elegir, dependiendo de los personajes instanciados
         int avatar2 = Teclado.nextInt();
@@ -84,7 +79,6 @@ public class App {
             case 6: System.out.println(avatarElegido + "Kreacher");
             break;
         }
-
         if( avatar1 == avatar2){
             System.out.println("Elija otro personaje. Seleccione avatar: '1' Harry, '2' Voldemort, '3' Hermione, '4' Bellatrix, '5' Dobby, '6' Kreacher");
             int avatar2bis = Teclado.nextInt();
@@ -101,16 +95,17 @@ public class App {
                 break;
                 case 6: System.out.println(avatarElegido + "Kreacher");
                 break;
-
+            }
         }
+    
         
 
-         
-        
+     }
 
-    }  
 }
-}
+
+    
+
 
 
         
